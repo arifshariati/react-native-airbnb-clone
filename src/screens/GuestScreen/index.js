02dcf4074dd734bb6a317e6c72206ab04f74f6f3
Arif Shariati <mohammad.arif.fast@hotmail.com>
2022-01-28 14:56:06 +0300

    Added Navigation Tab and some navigation screens

--- src/screens/GuestScreen/index.js
+++ src/screens/GuestScreen/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { Pressable, Text, View } from 'react-native';
 import guestScreenOptions from '../../../assets/data/guestScreenOptions';
 import { operation, optionTitle } from '../../../assets/data/constants';
@@ -7,6 +8,7 @@ import styles from './styles';
 
 
 const GuestScreen = () => {
+    const navigation = useNavigation();
     const [adults, setAdults] = useState(0);
     const [children, setChildren] = useState(0);
     const [infants, setInfants] = useState(0);
@@ -37,30 +39,39 @@ const GuestScreen = () => {
     }
 
     return (
-        <View>
-            {
-                guestScreenOptions.map(({ title, subTitle }, index) =>
-                    <View style={styles.row} key={index}>
-                        <View>
-                            <Text style={{ fontWeight: 'bold' }}>{title}</Text>
-                            <Text style={{ color: '#8d8d8d' }}>{subTitle}</Text>
+        <View style={{ justifyContent: 'space-between', height: '100%' }}>
+            <View>
+                {
+                    guestScreenOptions.map(({ title, subTitle }, index) =>
+                        <View style={styles.row} key={index}>
+                            <View>
+                                <Text style={{ fontWeight: 'bold' }}>{title}</Text>
+                                <Text style={{ color: '#8d8d8d' }}>{subTitle}</Text>
+                            </View>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <Pressable onPress={() => handleOperation(title, operation.SUBTRACT)} style={styles.button}>
+                                    <Text style={{ fontSize: 16, color: '#474747' }}>-</Text>
+                                </Pressable>
+                                <Text style={{ marginHorizontal: 20, fontSize: 16 }}>
+                                    {
+                                        title === optionTitle.ADULTS ? adults : title === optionTitle.CHILDREN ? children : infants
+                                    }
+                                </Text>
+                                <Pressable onPress={() => handleOperation(title, operation.ADD)} style={styles.button}>
+                                    <Text style={{ fontSize: 16, color: '#474747' }}>+</Text>
+                                </Pressable>
+                            </View>
                         </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Pressable onPress={() => handleOperation(title, operation.SUBTRACT)} style={styles.button}>
-                                <Text style={{ fontSize: 16, color: '#474747' }}>-</Text>
-                            </Pressable>
-                            <Text style={{ marginHorizontal: 20, fontSize: 16 }}>
-                                {
-                                    title === optionTitle.ADULTS ? adults : title === optionTitle.CHILDREN ? children : infants
-                                }
-                            </Text>
-                            <Pressable onPress={() => handleOperation(title, operation.ADD)} style={styles.button}>
-                                <Text style={{ fontSize: 16, color: '#474747' }}>+</Text>
-                            </Pressable>
-                        </View>
-                    </View>
-                )
-            }
+                    )
+                }
+            </View>
+
+            <Pressable
+                style={styles.searchPressable}
+            // onPress={()=> navigation.navigate('')}
+            >
+                <Text style={styles.searchPressableText}>Search</Text>
+            </Pressable>
 
         </View>
     );
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { View, Text, ImageBackground, Pressable } from 'react-native'
+import { View, Text, ImageBackground, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 const bgImage = require('../../../assets/images/wallpaper.jpg');
 
 const HomeScreen = () => {
 
+    const navigation = useNavigation();
     return (
 
         <View>
-            <Pressable onPress={() => console.warn('Search Button Clicked')} style={styles.searchButton}>
+            <Pressable
+                onPress={() => navigation.navigate('DestinationSearch')}
+                style={styles.searchButton}
+            >
                 <Fontisto name={'search'} size={25} color={'#f15454'} />
                 <Text style={styles.searchButtonText}>Where are you going?</Text>
             </Pressable>
